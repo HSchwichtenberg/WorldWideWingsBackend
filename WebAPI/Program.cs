e8270b767a7f15e8fa61faa27650f616a311fe35
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2022-12-05 22:18:09 +0100

    Text

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,46 +1,3 @@
-//namespace WebAPI;
-
-//public class Program
-//{
-// public static void Main(string[] args)
-// {
-//  var builder = WebApplication.CreateBuilder(args);
-
-//  // Add services to the container.
-
-//  builder.Services.AddControllers();
-//  // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//  builder.Services.AddEndpointsApiExplorer();
-//  builder.Services.AddSwaggerGen();
-
-//  //builder.Services.AddRazorPages();
-
-//  var app = builder.Build();
-
-//  // Configure the HTTP request pipeline.
-//  if (!app.Environment.IsDevelopment())
-//  {
-//   app.UseExceptionHandler("/Error");
-//   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//   app.UseHsts();
-//  }
-
-//  app.UseHttpsRedirection();
-//  app.UseStaticFiles();
-
-//  //app.UseRouting();
-
-//  app.UseHttpsRedirection();
-// app.UseAuthorization();
-
-//  app.MapControllers();
-//  //app.MapRazorPages();
-
-//  app.Run();
-// }
-//}
-
-
 
 namespace WebAPI;
 
@@ -62,11 +19,11 @@ public class Program
   var app = builder.Build();
 
   // Configure the HTTP request pipeline.
-  if (app.Environment.IsDevelopment())
-  {
+  //if (app.Environment.IsDevelopment()) // in diesem Fall auch öffentlich!
+  //{
    app.UseSwagger();
    app.UseSwaggerUI();
-  }
+  //}
 
   app.UseHttpsRedirection();
 
