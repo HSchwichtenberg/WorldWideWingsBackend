586edf0dab06c7195b108c907f1f26ba1a692fe7
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2022-12-06 11:44:56 +0100

    +CORS

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,3 @@
-
-using System.Runtime.CompilerServices;
-
 namespace WebAPI;
 
 public class Program
@@ -13,9 +10,14 @@ public class Program
   // Get Connection String
   DA.WWWingsContext.ConnectionString = builder.Configuration.GetConnectionString("WWWings");
 
- // Add services to the container.
+  #region Enable CORS 
+  builder.Services.AddCors();
+  #endregion
 
- builder.Services.AddControllers();
+
+  // Add services to the container.
+
+  builder.Services.AddControllers();
   builder.Services.AddRazorPages();
 
   // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -24,11 +26,23 @@ public class Program
 
   var app = builder.Build();
 
+  #region CORS
+  // Namespace: using Microsoft.AspNet.Cors;
+  app.UseCors(builder =>
+  builder.SetIsOriginAllowed((x) => true) // statt .AllowAnyOrigin(), notwendig für SignalR-JavaScript-Client
+         .AllowAnyHeader()
+         .AllowAnyMethod()
+         .AllowCredentials() // notwendig für SignalR-JavaScript-Client, siehe https://docs.microsoft.com/en-us/aspnet/core/signalr/javascript-client?view=aspnetcore-6.0
+          );
+
+  //.AllowCredentials() // InvalidOperationException: The CORS protocol does not allow specifying a wildcard (any) origin and 
+  #endregion
+
   // Configure the HTTP request pipeline.
   //if (app.Environment.IsDevelopment()) // in diesem Fall auch öffentlich!
   //{
-   app.UseSwagger();
-   app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
   //}
 
   app.UseHttpsRedirection();
